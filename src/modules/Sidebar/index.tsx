23102524b92e8699357e7dd331729813057ebf40
Amir Y <amir.youssefi@gmail.com>
2024-10-28 18:25:14 -0700

    name

--- src/modules/Sidebar/index.tsx
+++ src/modules/Sidebar/index.tsx
@@ -81,9 +81,9 @@ const Sidebar: FC<{
   return (
     <div className="flex flex-col h-full bg-white">
       <div className="p-4 mt-2 flex items-baseline justify-between">
-        <span className="text-3xl text-gradient font-[800]">ChatGPT</span>
+        <span className="text-3xl text-gradient font-[800]">LetsChatNow</span>
         <a
-          href="https://github.com/GPTGenius/chatgpt-vercel"
+          href="http://linkedin.com/in/youssefi"
           target="_blank"
           rel="noreferrer"
         >
