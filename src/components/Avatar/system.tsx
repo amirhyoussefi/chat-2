b73fa60f447440bf43c9ecddd574c6d04fce9882
Amir Y <amir.youssefi@gmail.com>
2024-12-04 17:11:14 -0800

    change left side chat icon

--- src/components/Avatar/system.tsx
+++ src/components/Avatar/system.tsx
@@ -7,13 +7,13 @@ interface SystemAvatarProps {
 const SystemAvatar: FC<
   SystemAvatarProps & React.ImgHTMLAttributes<HTMLImageElement>
 > = ({ role, ...props }) => {
-  let src = '/openai.webp';
+  let src = '/chat-ai-line-32.png';
   if (role === 'Midjourney') {
     src = '/midjourney.webp';
   } else if (role === 'Replicate') {
     src = '/replicate.svg';
   }
-  return <img width={32} height={32} {...props} src={src} />;
+  return <img width={24} height={24} {...props} src={src} />;
 };
 
 export default SystemAvatar;
